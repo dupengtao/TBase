6f24943847ce653902be3fc9af2dabf1a98feb04
dupengtao <dupengtao88@gmail.com>
2014-05-28 15:20:08 +0800

    restructure TBaseNetClent

--- TBase/src/com/dpt/tbase/app/base/engine/TBaseEngine.java
+++ TBase/src/com/dpt/tbase/app/base/engine/TBaseEngine.java
@@ -18,7 +18,7 @@ public class TBaseEngine {
     protected void excute(Context context, final IUiBaseResultCallBack<?> uiCb,
             AbStrResultCallBack abStrResultCallBack, String url) {
         try {
-            TBaseNetClent.executeRequest(context, url, null,TBaseNetClent.TYPE_JSON, abStrResultCallBack);
+            TBaseNetClent.executeRequest(context, url,TBaseNetClent.TYPE_TREADER_JSON, abStrResultCallBack);
         } catch (NetNotConnException e1) {
             uiCb.noNetworkEnvironment();
         } catch (NullPointerException e1) {
@@ -28,7 +28,7 @@ public class TBaseEngine {
     protected void excute4String(Context context, final IUiBaseResultCallBack<?> uiCb,
     		AbStrResultCallBack abStrResultCallBack, String url) {
     	try {
-    		TBaseNetClent.executeRequest(context, url, null,TBaseNetClent.TYPE_STRING, abStrResultCallBack);
+    		TBaseNetClent.executeRequest(context, url,TBaseNetClent.TYPE_STRING, abStrResultCallBack);
     	} catch (NetNotConnException e1) {
     		uiCb.noNetworkEnvironment();
     	} catch (NullPointerException e1) {
--- TBase/src/com/dpt/tbase/app/net/TBaseNetClent.java
+++ TBase/src/com/dpt/tbase/app/net/TBaseNetClent.java
@@ -1,18 +1,22 @@
 package com.dpt.tbase.app.net;
 
-import java.util.Map;
+import java.io.File;
 
 import org.json.JSONObject;
 
 import android.content.Context;
 import android.text.TextUtils;
-import android.widget.Toast;
+import android.widget.ImageView;
 
+import com.android.volley.Request;
 import com.android.volley.Request.Method;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.dpt.tbase.app.application.TBaseApplication;
 import com.dpt.tbase.app.base.exception.NetNotConnException;
@@ -26,101 +30,178 @@ import com.dpt.tbase.app.net.interfaces.INetStrClientCallBack;
  * 网络数据请求
  * 
  * @author dupengtao@cyou-inc.com
- * 
  *         2014-4-10
  */
 public abstract class TBaseNetClent {
 
-	private static String TAG = "TBaseNetClent";
-	public static final int TYPE_JSON = 1;
-	public static final int TYPE_STRING = 2;
+    private static String TAG = TBaseNetClent.class.getSimpleName();
+    public static final int TYPE_TREADER_JSON = 1;
+    public static final int TYPE_STRING = 2;
+    public static final int TYPE_JSON = 3;
 
-	public static void executeRequest(Context context, String url,
-			Map<String, String> params, int type,
-			INetBaseClientCallBack callBack) {
+    
+    public static void executeRequest(Request<?> request) {
+        executeRequest(true, null, request);
+    }
+    
+    public static void executeRequest(boolean isShouldCache, String resTag ,Request<?> request) {
+        addQueue(isShouldCache, resTag, request);
+    }
+    
+    public static void executeRequest(Context context, String url, int type,
+            INetBaseClientCallBack callBack) {
+        executeRequest(context, url, type, true, null, callBack);
+    }
 
-		if (!NetWorkUtil.checkNetWork(context)) {
-			LogHelper.e(TAG, "unable to connect to the network !");
-			callBack.onFinishCallBack();
-			throw new NetNotConnException("net not connected");
-		}
-		if (TextUtils.isEmpty(url)) {
-			LogHelper.e(TAG, "url is null or empty !");
-			throw new NullPointerException("url is null or empty !");
-		}
-		if (callBack == null) {
-			LogHelper.e(TAG, "callback not find !");
-			throw new NullPointerException("callback not find !");
-		}
+    public static void executeRequest(Context context, String url, int type,
+            boolean isShouldCache, String resTag, INetBaseClientCallBack callBack) {
+        check(context, url, callBack);
+        Request<?> request ;
+        if (TYPE_STRING == type) {
+            INetStrClientCallBack strClientCallBack = (INetStrClientCallBack) callBack;
+            request = getTBaseStringRequest(context, url, strClientCallBack);
+        } else if (TYPE_JSON == type) {
+            INetJsonClientCallBack jsonClientCallBack = (INetJsonClientCallBack) callBack;
+            request = getJsonRequest(context, url, jsonClientCallBack);
+        } else {// shoud use executeRequest(Request<?> request)
+            INetJsonClientCallBack jsonClientCallBack = (INetJsonClientCallBack) callBack;
+            request = getTReaderJsonRequest(context, url, jsonClientCallBack);
+        }
+        addQueue(isShouldCache, resTag, request);
+    }
 
-		if (TYPE_JSON == type) {
-		    INetJsonClientCallBack jsonClientCallBack=(INetJsonClientCallBack)callBack;
-			TBaseApplication.getInstance().addToRequestQueue(
-					getTBaseJsonRequest(context, url, jsonClientCallBack));
-		} else {
-		    INetStrClientCallBack strClientCallBack=(INetStrClientCallBack)callBack;
-			TBaseApplication.getInstance().addToRequestQueue(
-					getTBaseStringRequest(context, url, strClientCallBack));
-		}
+    public static void addQueue(boolean isShouldCache, String resTag, Request<?> request) {
+        if (!isShouldCache) {
+            request.setShouldCache(false);
+        }
+        if (TextUtils.isEmpty(resTag)) {
+            TBaseApplication.getInstance().addToRequestQueue(request);
+        } else {
+            TBaseApplication.getInstance().addToRequestQueue(request, resTag);
+        }
+    }
 
-	}
+    public static void check(Context context, String url, INetBaseClientCallBack callBack) {
+        if (!NetWorkUtil.checkNetWork(context)) {
+            LogHelper.e(TAG, "unable to connect to the network !");
+            callBack.onFinishCallBack();
+            throw new NetNotConnException("net not connected");
+        }
+        if (TextUtils.isEmpty(url)) {
+            LogHelper.e(TAG, "url is null or empty !");
+            throw new NullPointerException("url is null or empty !");
+        }
+        if (callBack == null) {
+            LogHelper.e(TAG, "callback not find !");
+            throw new NullPointerException("callback not find !");
+        }
+    }
 
-	private static TBaseJsonObjectRequest getTBaseJsonRequest(
-			final Context context, String url,
-			final INetJsonClientCallBack callBack) {
-		callBack.onStartCallBack();
-		TBaseJsonObjectRequest request = new TBaseJsonObjectRequest(Method.GET,
-				url, null, new Listener<JSONObject>() {
+    private static JsonObjectRequest getJsonRequest(
+            final Context context, String url,
+            final INetJsonClientCallBack callBack) {
+        callBack.onStartCallBack();
+        JsonObjectRequest request = new JsonObjectRequest(Method.GET,
+                url, null, new Listener<JSONObject>() {
 
-					@Override
-					public void onResponse(JSONObject response) {
-						callBack.onSuccessCallBack(response);
-						callBack.onFinishCallBack();
-					}
-				}, new ErrorListener() {
-					@Override
-					public void onErrorResponse(VolleyError error) {
-						processError(context, callBack, error);
-					}
-				});
-		return request;
-	}
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        callBack.onSuccessCallBack(response);
+                        callBack.onFinishCallBack();
+                    }
+                }, new ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        processError(context, callBack, error);
+                    }
+                });
+        return request;
+    }
 
-	private static StringRequest getTBaseStringRequest(final Context context,
-			String url, final INetStrClientCallBack callBack) {
-		callBack.onStartCallBack();
+    private static TBaseJsonObjectRequest getTReaderJsonRequest(
+            final Context context, String url,
+            final INetJsonClientCallBack callBack) {
+        callBack.onStartCallBack();
+        TBaseJsonObjectRequest request = new TBaseJsonObjectRequest(Method.GET,
+                url, null, new Listener<JSONObject>() {
 
-		StringRequest stringRequest = new StringRequest(url,
-				new Response.Listener<String>() {
-					@Override
-					public void onResponse(String response) {
-						callBack.onSuccessCallBack(200,response.toString(),null);
-						callBack.onFinishCallBack();
-					}
-				}, new Response.ErrorListener() {
-					@Override
-					public void onErrorResponse(VolleyError error) {
-						processError(context, callBack, error);
-					}
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        callBack.onSuccessCallBack(response);
+                        callBack.onFinishCallBack();
+                    }
+                }, new ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        processError(context, callBack, error);
+                    }
+                });
+        return request;
+    }
 
-				});
+    private static StringRequest getTBaseStringRequest(final Context context,
+            String url, final INetStrClientCallBack callBack) {
+        callBack.onStartCallBack();
 
-		return stringRequest;
-	}
+        StringRequest stringRequest = new StringRequest(url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        callBack.onSuccessCallBack(200, response.toString(), null);
+                        callBack.onFinishCallBack();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        processError(context, callBack, error);
+                    }
 
-	private static void processError(final Context context,
-			final INetBaseClientCallBack callBack, VolleyError error) {
-		String message = "";
-		try {
-			message = VolleyErrorHelper.getMessage(error, context);
-			message += "statusCode" + error.networkResponse.statusCode;
-			Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally{
-			callBack.onFailureCallBack(error, message);
-			callBack.onFinishCallBack();
-		}
+                });
 
-	}
+        return stringRequest;
+    }
+
+    private static void processError(final Context context,
+            final INetBaseClientCallBack callBack, VolleyError error) {
+        String message = "";
+        try {
+            message = VolleyErrorHelper.getMessage(error, context);
+            message += "statusCode" + error.networkResponse.statusCode;
+            LogHelper.e(TAG, message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            callBack.onFailureCallBack(error, message);
+            callBack.onFinishCallBack();
+        }
+    }
+
+    public static void executeImageRequest(String requestUrl, ImageView imageView, int roadingResId,
+            int errorResId) {
+        TBaseApplication.getInstance().getImageLoader().get(requestUrl, ImageLoader.getImageListener(
+                imageView, roadingResId, errorResId));
+    }
+    
+    public static void cacheRemove(String url){
+        TBaseApplication.getInstance().getRequestQueue().getCache().remove(url);
+    }
+    public static void cacheClear(){
+        TBaseApplication.getInstance().getRequestQueue().getCache().clear();
+    }
+    public static void cancelSingleRequest(String reqTag){
+        TBaseApplication.getInstance().getRequestQueue().cancelAll(reqTag);
+    }
+    public static void cancelAllRequests(){
+        TBaseApplication.getInstance().getRequestQueue().cancelAll(new RequestQueue.RequestFilter() {
+            @Override
+            public boolean apply(Request<?> request) {
+                return true;
+            }
+        });
+    }
+    
+    public static long getCacheSize(Context context){
+        File cacheDir = new File(context.getCacheDir(), "volley");
+        return cacheDir.length();
+    }
 }
